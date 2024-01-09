6f30c488f359a1b71ac03a194149201f2a3554aa
chen weichen <newsforcwc@gmail.com>
2024-01-09 10:41:11 +0800

    minor updates

--- admin/admin.js
+++ admin/admin.js
@@ -1,28 +1,34 @@
-const base = (app) => { app.get("/", (req,res) => {
-	return res.json("front page");
-})}
+const base = (app) => { 
+	app.get("/", (req,res) => {
+		return res.json("front page");
+	})
+}
 
-const allUser = (app,db) => { app.get("/allUser", (req,res) => {
-	db.select("*").from("users")
-		.then(user => {
-			if (user.length) {
-				res.json(user);
-			} else {
-				res.json("no user");
-			}
-		})
-		.catch(err => res.status(400).json("server issue"))
-})}
+const allUser = (app,db) => { 
+	app.get("/allUser", (req,res) => {
+		db.select("*").from("users")
+			.then(user => {
+				if (user.length) {
+					res.json(user);
+				} else {
+					res.json("no user");
+				}
+			})
+			.catch(err => res.status(400).json("server issue"))
+	})
+}
 
-const deleteAll = (app,db) => { app.delete("/deleteAll", (req,res) => {
-	db("users")
-		.returning("name")
-		.del()
-		.then(() => {
-			res.json("all data is deleted"); 
-		})
-		.catch(err => res.status(400).json("something went wrong"));
-})}
+const deleteAll = (app,db) => { 
+	app.delete("/deleteAll", (req,res) => {
+		db("users")
+			.returning("name")
+			.del()
+			.then(() => {
+				res.json("all data is deleted"); 
+			})
+			.catch(err => res.status(400).json("something went wrong"));
+	})
+}
 
 const admin = (app, db) => {
 	base(app);
